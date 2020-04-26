b554b244221bc37f547c763a3ff3c0ed4a6dbfa3
alejandromartincruz <socorreitor@gmail.com>
2020-04-26 14:16:38 +0200

    refactorizado router segun feedback y creado nuevo test para ruta correcta

--- src/Router/Domain/Router.php
+++ src/Router/Domain/Router.php
@@ -2,29 +2,99 @@
 
 namespace Router\Domain;
 
-use Router\Application\MatchRouteUseCase;
-
 class Router
 {
-    private $matchRouteUseCase;
-
-    public function __construct(
-        MatchRouteUseCase $matchRouteUseCase
-    )
-    {
-        $this->matchRouteUseCase = $matchRouteUseCase;
-    }
+    const ROUTE_COLLECTION = array(
+            '/',
+            '/post/{id}/',
+            '/post/'
+        );
 
     public function get($route, $method)
     {
-        $result = $this->matchRouteUseCase->execute($route);
+        $result = $this->match($route);
         $match = $result['match'];
         $args = $result['args'];
         if ($match) {
-            echo call_user_func_array($method, array()) . PHP_EOL;
-            return;
+            return call_user_func_array($method, array());
+            // return;
         }
 
         throw new \Exception("$route - Route not found: Error 404");
+    }
+
+    public function match(string $requestedRoute): array
+    {
+        $routes = self::ROUTE_COLLECTION;
+
+        foreach ($routes as $route) {
+            if ($this->isMatch($requestedRoute, $route)) {
+                $args = $this->extractVariable($requestedRoute, $route);
+                return array(
+                    "match" => true,
+                    "args"  => $args
+                );
+            }
+        }
+
+        return array(
+            "match" => false,
+            "args"  => []
+        );
+
+    }
+
+    private function isMatch(string $requestedRoute, string $route): bool
+    {
+        if ($route === $requestedRoute) {
+            return true;
+        }
+
+        if ($route === '/') {
+            return false;
+        }
+
+        $cleanRoute = rtrim($route,"/");
+        $cleanRoute = preg_replace('/{[\s\S]+?}/', '', $cleanRoute);
+
+        if (strpos($requestedRoute, $cleanRoute) !== false) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function extractVariable($uri, $regexUri): array
+    {
+        $params = [];
+        preg_match_all('\'' . '{(\w+)}' . '\'', $regexUri, $matches);
+
+        $matches = $matches[0];
+
+        foreach ($matches as $key => $value)
+        {
+            $matches[$key] = str_replace('{', '', $matches[$key]);
+            $matches[$key] = str_replace('}', '', $matches[$key]);
+        }
+
+        $regexUri = preg_replace('%' . '{(\w+)}' . '%', '(\w+|\d+)', $regexUri);
+
+        $regexUri .= '$';
+        $regexUri = '%^' . $regexUri . '%';
+        $res = preg_match($regexUri, $uri, $params);
+
+        if (!$res || $res == 0 )
+        {
+            return array();
+        }
+
+        $paramLength = count($matches);
+        $keyParams = array();
+        for ($i=0; $i < $paramLength; $i++)
+        {
+            $keyParams[$matches[$i]] = $params[$i+1];
+        }
+
+        return $keyParams;
     }
 }
--- src/Router/app.php
+++ src/Router/app.php
@@ -1,14 +1,14 @@
 <?php
 
+// este archivo no se pide para la practica, se ha generado exclusivamente para comprobar el correcto funcionamiento de la clase Router antes de desarrollar los tests
+
 namespace Router;
 
 require __DIR__.'/../../vendor/autoload.php';
 
-use Router\Application\ExtractVariableUseCase;
-use Router\Application\MatchRouteUseCase;
 use Router\Domain\Router;
 
-$router = new Router(new MatchRouteUseCase(new ExtractVariableUseCase()));
+$router = new Router();
 
 $router->get('/post/5/', function() {
     return <<<HTML
